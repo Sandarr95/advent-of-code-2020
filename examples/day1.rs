86c2deb95f50ffed2a5342d08b6ed9f6d849ab88
Sander Kolman <Sandarr95@users.noreply.github.com>
2020-12-12 21:10:35 +0100

    implemented part 2, improved part 1 a little

--- examples/day1.rs
+++ examples/day1.rs
@@ -2,6 +2,10 @@ use aocf::Aoc;
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
+fn to_u32(x: &str) -> Option::<u32> {
+  x.parse::<u32>().ok()
+}
+
 fn main() {
   let mut aoc = Aoc::new()
               .year(Some(2020))
@@ -12,14 +16,27 @@ fn main() {
   let input = aoc.get_input(false);
 
   if let Ok(i) = input {
-    let ints_iter = i.split("\n").filter(|&x| x != "").map(|x| x.parse::<u32>().unwrap());
+    let ints_iter = i.split("\n").filter_map(to_u32);
     let set_of_ints = HashSet::<u32>::from_iter(ints_iter.clone());
-    for part_of_2020 in ints_iter {
+    for part_of_2020 in ints_iter.clone() {
       let guess_of_2020 = 2020 - part_of_2020;
       if set_of_ints.contains(&guess_of_2020) {
-        return println!("{}", guess_of_2020 * part_of_2020);
+        println!("Part 1: {}", guess_of_2020 * part_of_2020);
+        break;
+      }
+    }
+
+    let ints_vec = Vec::from_iter(ints_iter);
+    for part1_of_2020 in &ints_vec {
+      for part2_of_2020 in &ints_vec {
+        for part3_of_2020 in &ints_vec {
+          //print!("{}, ", part1_of_2020 + part2_of_2020 + part3_of_2020);
+          if part1_of_2020 + part2_of_2020 + part3_of_2020 == 2020 {
+            println!("Part 2: {}", part1_of_2020 * part2_of_2020 * part3_of_2020);
+            return;
+          }
+        }
       }
     }
   }
-
 }
